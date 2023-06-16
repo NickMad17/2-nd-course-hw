0a194c8b56f9edce4525970366d5cb6c3f774a0b
Maady <vlad030911@gmail.com>
2023-06-17 00:12:47 +0300

    append new layout and scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// -------- del hover ----------
+
 function hasTouch() {
     return 'ontouchstart' in document.documentElement
            || navigator.maxTouchPoints > 0
@@ -22,3 +24,15 @@ if (hasTouch()) { // remove all :hover stylesheets
         
     }
 };
+
+// -------- scrollReveal ----------
+
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 1500,
+    delay: 100
+});
+
+ScrollReveal().reveal('.header__title, .header__text, .one__left, .two__left, .three p, .three h1', { origin: 'top' });
+ScrollReveal().reveal('.games, .one__right , .two__right, .three a, .header a ', { origin: 'bottom' });
