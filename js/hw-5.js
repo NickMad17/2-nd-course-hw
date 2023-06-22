3123401d2737ce5c6dc37612ff84c10e72bbf471
Maady <vlad030911@gmail.com>
2023-06-22 19:56:01 +0300

    add 8 exercise

--- js/hw-5.js
+++ js/hw-5.js
@@ -26,7 +26,6 @@
 // console.log(squareNumber(3));
 // squareNumberTwo(4);
 
-
 // 4
 
 // function hello (){
@@ -44,55 +43,77 @@
 
 // // 5
 
-// const multiplyNumb = (a,b) => {
-//     if (isNaN(a) || isNaN(b)){
-//         console.log("Одно или оба значения не являются числом")
-//     } else{
-//         console.log(a*b)
-//     }
-// }
-
-// multiplyNumb(1,"eokfp");
+// const multiplyNumb = (a, b) => {
+//   if (isNaN(a) || isNaN(b)) {
+//     console.log("Одно или оба значения не являются числом");
+//   } else {
+//     console.log(a * b);
+//   }
+// };
+//
+// multiplyNumb(1, "eokfp");
 
 // // 6
 
 // function cube (number){
 //     if(!isNaN(number)){
 //         return Math.pow(number,3);
-//     } 
+//     }
 // }
 
 // console.log(cube(35));
 
 // 7
 
-function perimetrCircle() {
-  return 2 * this.radius * Math.PI;
-}
-function squareCircle() {
-  return Math.PI * this.radius ** 2;
-}
+// function getPerimeter() {
+//     return 2 * this.radius * Math.PI;
+// }
+//
+// function getArea() {
+//     return Math.PI * this.radius ** 2;
+// }
+//
+// const circle1 = {
+//     radius: 3,
+//     getPerimeter: getPerimeter,
+//     getArea: getArea,
+// };
+//
+// const circle2 = {
+//     radius: 3,
+//     getPerimeter: getPerimeter,
+//     getArea: getArea,
+// }
+// ;
+//
+// console.log(
+//     `Периметр круга №1: ${circle1.getPerimeter()} м, Площадь: ${circle1.getArea()} м^2,\n
+//     Периметр круга №2: ${circle2.getPerimeter()} м, Площадь: ${circle2.getArea()} м^2`
+// );
 
-const circle1 = {
-  radius: 3,
-  perimetrCircle: perimetrCircle,
-  squareCircle: squareCircle,
+// 8
+
+const month = { // обьект содержащий => название месяца -> Время года
+    1: "Зима",
+    2: "Зима",
+    3: "Весна",
+    4: "Весна",
+    5: "Весна",
+    6: "Лето",
+    7: "Лето",
+    8: "Лето",
+    9: "Осень",
+    10: "Осень",
+    11: "Осень",
+    12: "Зима"
 };
 
-const circle2 = {
-  radius: 3,
-  perimetrCircle: perimetrCircle,
-  squareCircle: squareCircle,
-};
+function getSseason() {
+    let mouthNumber = +prompt("Введите номер месяца");
 
-console.log(
-  circle1.perimetrCircle(),
-  circle2.perimetrCircle(),
-  circle1.squareCircle(),
-  circle2.squareCircle()
-);
-
-
-
-
-
+    if(isNaN(mouthNumber) || mouthNumber > 12 || mouthNumber <= 0){
+        alert("Нужно ввести число от 1 до 12");
+    } else{
+        alert(`Время года: ${month[mouthNumber]}`);
+    }
+}
